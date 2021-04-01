2c320bc5d0031cdf26df93a51491a356718c58cc
Marco Ferragina <marco.ferragina@gmail.com>
2021-04-01 18:36:32 +0200

    Delete ui/src/interfaces directory

--- ui/src/interfaces/Fs.ts
+++
@@ -1,6 +0,0 @@
-export interface FsItem {
-    name: string,
-    isDir: boolean,
-    // if there is python instance running or not
-    isRunning: boolean
-}
--- ui/src/interfaces/WSMessage.ts
+++
@@ -1,7 +0,0 @@
-export interface WSMessage {
-    cellID: number,
-    runID: number
-    data: string,
-    channel: string,
-    contentType: string
-}
