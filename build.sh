afd09e75a607a3e35f2c71b511270f50f908eabe
Marco Ferragina <marco.ferragina@gmail.com>
2021-04-02 09:26:09 +0200

    releaser

--- build.sh
+++ build.sh
@@ -2,4 +2,14 @@
 
 cd ui && npm install && npm run build && cd ..
 
-go build -o ./bin/shellme ./cmd/shellme
+build() {
+    EXT=""
+    [[ $GOOS = "windows" ]] && EXT=".exe"
+    go build -o ./bin/shellme-${GOOS}-${GOARCH}${EXT} ./cmd/shellme
+}
+
+GOOS=linux GOARCH=arm build
+GOOS=linux GOARCH=amd64 build
+
+GOOS=darwin GOARCH=amd64 build
+GOOS=darwin GOARCH=arm64 build
