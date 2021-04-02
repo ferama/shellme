b965cbb91eb8fb51b45bd024a7043b0f34c9885f
Marco Ferragina <marco.ferragina@gmail.com>
2021-04-02 09:37:20 +0200

    enabled linux arm64 target

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
-cd ui && npm install && npm run build && cd ..
+# cd ui && npm install && npm run build && cd ..
 
 build() {
     EXT=""
     [[ $GOOS = "windows" ]] && EXT=".exe"
+    echo "Building ${GOOS} ${GOARCH}"
     go build -o ./bin/shellme-${GOOS}-${GOARCH}${EXT} ./cmd/shellme
 }
 
 GOOS=linux GOARCH=arm build
+GOOS=linux GOARCH=arm64 build
 GOOS=linux GOARCH=amd64 build
 
 GOOS=darwin GOARCH=amd64 build
